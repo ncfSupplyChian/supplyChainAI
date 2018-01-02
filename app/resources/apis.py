403773d7814f70e1e40eb6df472f1cbfad62691a
李强 <liqiang7@ucfgroup.com>
2018-01-02 18:34:17 +0800

    增加kmeans逻辑

--- app/resources/apis.py
+++ app/resources/apis.py
@@ -7,40 +7,25 @@ from sklearn import metrics
 
 bp = Blueprint('apis', __name__)
 
-tasks =[
-    {
-        'id':1,
-        'title': 'Buy groceries',
-        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': 'Learn Python',
-        'description': 'Need to find a good Python tutorial on the web',
-        'done': False
-    }]
 
-
-@bp.route('/api/v1/test', methods=['GET'])
-def test():
+@bp.route('/api/v1/kmeans/<kvalue>', methods=['GET'])
+def kmeans(kvalue):
     dataMat = []
-    fr = open("E:\\code\\python\\kmeans\\testset4.txt")  # 注意，这个是相对路径，请保证是在 MachineLearning 这个目录下执行。
+    fr = open("E:\\code\\python\\kmeans\\testset4.txt")
     for line in fr.readlines():
         curLine = line.strip().split('\t')
         fltLine = list(map(float, curLine))  # 映射所有的元素为 float（浮点数）类型
         dataMat.append(fltLine)
-    km = KMeans(n_clusters=24)  # 初始化
+    km = KMeans(n_clusters=int(kvalue))  # 初始化
     km.fit(dataMat)  # 拟合
-    km_pred = km.predict(dataMat)  # 预测
-    centers = km.cluster_centers_  # 质心
-    # for temp in centers:
-    #     for tempPoint in temp:
-    #         print(tempPoint)
-    #     print("")
-    f = open("E:\\code\\python\\kmeans\\result.txt", "w")
-    for test in km_pred:
-        print(test, file=f)
-    f.close()
-    return jsonify({'tasks': tasks})
+    km_preds = km.predict(dataMat).tolist()  # 预测
+    centers = km.cluster_centers_.tolist()  # 质心
+    result_list = []
+    for i, centerPoint in enumerate(centers):
+        result = {'kIndex': i, 'centerPoint': centerPoint, 'dataCount': 0}
+        result_list.append(result)
+    for km_pred in km_preds:
+        result_list[km_pred]['dataCount'] = result_list[km_pred]['dataCount'] + 1
+    ch_value = metrics.calinski_harabaz_score(dataMat, km_preds)
+    return jsonify({'resultList': result_list, 'chValue': ch_value})
 
