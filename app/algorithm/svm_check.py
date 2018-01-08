a7b4281bc2edd8524b8a934220388cc09c7fb492
李强 <liqiang7@ucfgroup.com>
2018-01-08 18:08:17 +0800

    增加SVM逻辑

---
+++ app/algorithm/svm_check.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split, cross_val_score
+
+# 用pandas加载数据.csv文件，然后用train_test_split分成训练集和测试集
+data = pd.read_csv('E:\\code\\python\\data\\dshl_lr.csv')
+feature_cols = ['最近登录间隔', '历史月均订单数量', '交易品牌数', '客单价', '注册时间长度',
+                '近6个月月均交易笔数', '近6个月月均交易SKU数', '近6个月月均交易金额', '店商互联信用得分', '取消订单次数']
+x = data[feature_cols]
+y = data['是否有退货']
+
+# 归一化
+print(x.iloc[0])
+x = preprocessing.scale(x)
+print(x[0])
+# x = preprocessing.MinMaxScaler().fit_transform(x)
+# print(x[0])
+# x = preprocessing.MaxAbsScaler().fit_transform(x)
+# print(x[0])
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.9)
+
+# 用支持向量机分类
+svm = SVC()
+svm.fit(X_train, y_train)
+predictions = svm.predict(X_test)
+
+print('测试集score：', svm.score(X_test, y_test))
+
+# for i, prediction in enumerate(predictions[-1:]):
+#     print('预测类型：%s. 信息: %s' % (prediction, X_test.iloc[i]))
+
+# 效果评估：
+# 准确率：scikit-learn提供了accuracy_score来计算：LogisticRegression.score()
+# 准确率是分类器预测正确性的比例，但是并不能分辨出假阳性错误和假阴性错误
+scores = cross_val_score(svm, X_train, y_train, cv=5)
+print('准确率：', np.mean(scores), scores)
+# 精确率和召回率：
+# 精确率是指分类器预测出的有退货小B中真的是退货小B的比例，P=TP/(TP+FP)
+# 召回率在医学上也叫做灵敏度，在本例中知所有真的退货小B被分类器正确找出来的比例，R=TP/(TP+FN)
+precisions = cross_val_score(svm, X_train, y_train, cv=5, scoring='precision')
+print('精确率：', np.mean(precisions), precisions)
+recalls = cross_val_score(svm, X_train, y_train, cv=5, scoring='recall')
+print('召回率：', np.mean(recalls), recalls)
+# 综合评价指标（F1 measure）是精确率和召回率的调和均值（harmonic mean），或加权平均值，也称为F-measure或fF-score。
+f1s = cross_val_score(svm, X_train, y_train, cv=5, scoring='f1')
+print('综合评价指标：', np.mean(f1s), f1s)
+roc_auc = cross_val_score(svm, X_train, y_train, cv=5, scoring='roc_auc')
+print('roc_auc：', np.mean(roc_auc), roc_auc)
+
---
+++ app/algorithm/svm_grid.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn import preprocessing
+from sklearn.metrics import precision_score, recall_score, accuracy_score, classification_report
+
+if __name__ == '__main__':
+    # 用pandas加载数据.csv文件，然后用train_test_split分成训练集和测试集
+    data = pd.read_csv('E:\\code\\python\\data\\dshl_lr.csv')
+    feature_cols = ['最近登录间隔', '历史月均订单数量', '交易品牌数', '客单价', '注册时间长度',
+                    '近6个月月均交易笔数', '近6个月月均交易SKU数', '近6个月月均交易金额', '店商互联信用得分', '取消订单次数']
+    x = data[feature_cols]
+    y = data['是否有退货']
+    x = preprocessing.scale(x)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.9)
+
+    # 网格搜索（Grid search）就是用来确定最优超参数的方法。
+    # 其原理就是选取可能的参数不断运行模型获取最佳效果。
+    # 网格搜索用的是穷举法，其缺点在于即使每个超参数的取值范围都很小，
+    # 计算量也是巨大的。不过这是一个并行问题，参数与参数彼此独立，
+    # 计算过程不需要同步，所有很多方法都可以解决这个问题。scikit-learn有GridSearchCV()函数解决这个问题：
+    pipeline = Pipeline([
+        ('clf', SVC())
+    ])
+    parameters = {
+        'clf__C': (0.1, 1, 10),
+        'clf__gamma': ('auto', 1, 0.1, 0.01),
+        # 'clf__kernel': ('linear', 'poly', 'rbf', 'sigmoid'),
+        'clf__kernel': ('rbf', 'sigmoid'),
+    }
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, scoring='accuracy', cv=5)
+    grid_search.fit(X_train, y_train)
+    print(grid_search.cv_results_)
+    print('最佳效果：%0.3f' % grid_search.best_score_)
+    print('最优参数组合：')
+    best_parameters = grid_search.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print('\t%s: %r' % (param_name, best_parameters[param_name]))
+    predictions = grid_search.predict(X_test)
+    print('准确率：', accuracy_score(y_test, predictions))
+    print('精确率：', precision_score(y_test, predictions))
+    print('召回率：', recall_score(y_test, predictions))
+    print(classification_report(y_test, predictions))
