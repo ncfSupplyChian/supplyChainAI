968d9509e57518c2605035153f20019b07ba93fc
李强 <liqiang7@ucfgroup.com>
2018-01-10 12:20:53 +0800

    增加KS曲线

--- app/algorithm/gbdt_grid.py
+++ app/algorithm/gbdt_grid.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
